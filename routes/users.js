8ba254239e4035ab95f233a0c4ebbbfdb790cef9
Wanjin Yoo <dksladjsla@gmail.com>
2020-11-29 07:04:19 +0000

    retrieve user data from database to html-page

--- routes/users.js
+++ routes/users.js
@@ -21,5 +21,18 @@ module.exports = (db) => {
           .json({ error: err.message });
       });
   });
+
+  router.get("/favourites", (req, res) => {
+    db.query(`SELECT * FROM user_favourites;`)
+      .then(data => {
+        const users = data.rows;
+        res.json(users);
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ error: err.message });
+      });
+  });
   return router;
 };
