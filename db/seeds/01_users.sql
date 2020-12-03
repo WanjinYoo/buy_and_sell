0436226d82095b617d0b38b4adda8b29d8f99e5f
Rahul <rahul.shial@gmail.com>
2020-12-03 01:49:13 +0000

    database updates

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,3 +1,14 @@
--- Users table seeds here (Example)
-INSERT INTO users (name) VALUES ('Alice');
-INSERT INTO users (name) VALUES ('Kira');
+insert into users (name, email, phone, is_admin)
+VALUES ('Stephan', 'admin@admin.com', '1234567890', 'Y');
+
+INSERT INTO users (name, email, phone)
+VALUES  ('Rahul', 'rahul.shial@gmail.com', '+15878892503'),
+  ('Wanjin', 'wanjin@yoo.com', '+12502179995'),
+  ('Dominic Parks','victoriablackwell@outlookcom', '+19514091382'),
+  ('Eva Stanley','sebastianguerra@ymailcom', '1234567890'),
+  ('Louisa Meyer','jacksonrose@hotmailcom', '1234567890'),
+  ('Sue Luna','jasonvincent@gmxcom', '1234567890'),
+  ('Rosalie Garza','jacksondavid@gmxcom', '1234567890'),
+  ('Etta West','charlielevy@yahoocom', '1234567890'),
+  ('Margaret Wong','makaylaweiss@icloudcom', '1234567890'),
+  ('Leroy Hart','jaycereynolds@inboxcom', '1234567890');
