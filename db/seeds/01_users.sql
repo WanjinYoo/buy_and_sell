98308a092fc427ab81bbb9cbc8c12ff15f8c7835
Rahul <rahul.shial@gmail.com>
2020-12-05 02:29:34 +0000

    Table Data cleanup

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -2,9 +2,9 @@ insert into users (name, email, phone, is_admin)
 VALUES ('Stephan', 'admin@admin.com', '1234567890', 'Y');
 
 INSERT INTO users (name, email, phone)
-VALUES  ('Rahul', 'rahul.shial@gmail.com', '+15878892503'),
-  ('Wanjin', 'wanjin@yoo.com', '+15878892503'),
-  ('Dominic Parks','victoriablackwell@outlookcom', '+15878892503'),
+VALUES  ('Rahul', 'random@somemail.com', '+1234567890'),
+  ('Wanjin', 'random@somemail.com', '+1234567890'),
+  ('Dominic Parks','victoriablackwell@outlookcom', '+1234567890'),
   ('Eva Stanley','sebastianguerra@ymailcom', '1234567890'),
   ('Louisa Meyer','jacksonrose@hotmailcom', '1234567890'),
   ('Sue Luna','jasonvincent@gmxcom', '1234567890'),
